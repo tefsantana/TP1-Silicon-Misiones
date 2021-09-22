d8fffcc42c84ad4ac1946c3351244ffa6a764788
Estefanía Santana <83146564+tefsantana@users.noreply.github.com>
2021-09-22 01:54:35 -0300

    Add files via upload

---
+++ PoloticMisiones/src/logica/PoloticMisiones.java
@@ -0,0 +1,18 @@
+
+package logica;
+
+import igu.Principal;
+
+/**
+ *
+ * @author Estefanía Santana
+ */
+public class PoloticMisiones {
+
+    public static void main(String[] args) {
+           Principal i = new Principal();   // creo mi pantalla
+           i.setVisible(true);   // la hago visible
+           i.setLocationRelativeTo(null);      // la centro
+    }
+    
+}
